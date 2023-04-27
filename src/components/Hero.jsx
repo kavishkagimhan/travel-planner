b683ee8851615821423e1a68e84bfca62c0e8915
Kavishka <it20094218@my.sliit.lk>
2023-04-27 10:41:21 +0530

    navbaar and hero change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Hero() {
   return (
     // style={{ backgroundImage: `url(${background})` }}
-    <div className='relative z-0 h-screen text-white bg-black bg-center bg-no-repeat bg-cover' style={{ backgroundImage: `url(${background})` }}>
+    <div className='relative h-screen text-white bg-black bg-center bg-no-repeat bg-cover -z-10' style={{ backgroundImage: `url(${background})` }}>
       <div className="absolute top-0 left-0 w-full h-screen bg-black opacity-50 -z-20"></div>
       <div className='flex items-center justify-center h-[70vh] text-center'>
         <div className='text-center'>
@@ -14,7 +14,7 @@ function Hero() {
             the ultimate travel companion website.</p>
             <div className='flex justify-between bg-white max-w-[500px] mx-auto text-gray-400 px-4 py-2 mt-4 items-center rounded-md'>
               <p className='font-thin'>What Whould You Like</p>
-              <Link to='/dashboard'><button className='px-4 py-2 text-white bg-blue-400 border rounded-md hover:bg-transparent hover:border-blue-400 hover:text-blue-400'>Start Planing</button></Link>
+              <Link to='/dashboard'><button className='px-4 py-2 text-white bg-blue-400 border rounded-md cursor-pointer hover:bg-transparent hover:border-blue-400 hover:text-blue-400'>Start Planing</button></Link>
             </div>
         </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
 
 function Navbar() {
+
+    const [nav, setNav] = useState(true);
+
+    const handleNav = () => {
+        setNav(!nav);
+    }
     return (
-        <div className='w-screen bg-white shadow-md'>
-            <div className='flex items-center justify-between h-12 bg-white text-uppercase max-w-[1340px] mx-auto py-10'>
-                <h1 className='text-3xl font-bold text-black cursor-auto font-inter'><Link to='/'>TRAVEL<span className='text-blue-500'>PLANNER</span></Link></h1>
-                <ul className='flex items-start gap-6 px-4 py-2 text-sm font-semibold text-black uppercase'>
+        <div className="bg-white mx-auto flex items-center justify-between h-20 text-black max-w-[1340px] px-4 z-60 ">
+            <Link to='/'> <h1 className='w-full text-3xl font-bold cursor-pointer'>TRAVEL<span className='text-blue-500'>PLANNER</span></h1></Link>
+            <ul className='hidden gap-6 font-sans uppercase md:flex'>
+                <li className='cursor-pointer hover:text-blue-500'><Link to='howworks'>How it works</Link></li>
+                <li className='cursor-pointer hover:text-blue-500'><Link to='/dashboard'>start Planning</Link></li>
+                <li className='cursor-pointer hover:text-blue-500'><Link to='https://www.oretatravels.com/'>Blog</Link></li>
+                <li className='cursor-pointer hover:text-blue-500'><Link to='reviews'>Reviews</Link></li>
+                <li className='cursor-pointer hover:text-blue-500'><Link to='contactus'>Contact</Link></li>
+                <li className='cursor-pointer hover:text-blue-500'><Link to='/conditions'>Terms and Conditions</Link></li>
+                <div className='flex items-center gap-4'>
+                <button className='px-4 py-2 text-white uppercase bg-blue-400 border rounded-md hover:bg-transparent hover:text-blue-400'><Link to='/login'>Login</Link></button>
+                <button className='px-4 py-2 text-blue-400 uppercase border border-blue-400 rounded-md hover:bg-blue-400 hover:text-white'><Link to='/signup'>SignUp</Link></button>
+            </div>
+            </ul>
+           
+            <div onClick={handleNav} className='block md:hidden'>
+                {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+            <div className={!nav ? 'fixed top-0 left-0 w-[60%] h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] ease-end-out duration-50 text-white'}>
+            <Link to='/'> <h1 className='w-full text-3xl font-bold text-white cursor-pointer'>TRAVEL<span className='text-blue-500'>PLANNER</span></h1></Link>
+                <ul className='flex flex-col gap-4 p-4 pt-24 text-white uppercase'>
                     <li className='cursor-pointer hover:text-blue-500'><Link to='howworks'>How it works</Link></li>
                     <li className='cursor-pointer hover:text-blue-500'><Link to='/dashboard'>start Planning</Link></li>
                     <li className='cursor-pointer hover:text-blue-500'><Link to='https://www.oretatravels.com/'>Blog</Link></li>
@@ -16,7 +40,7 @@ function Navbar() {
                     <li className='cursor-pointer hover:text-blue-500'><Link to='/conditions'>Terms and Conditions</Link></li>
                 </ul>
                 <div className='flex items-center gap-4'>
-                <button className='px-4 py-2 text-white uppercase bg-blue-400 border rounded-md hover:bg-transparent hover:text-blue-400'><Link to='/login'>Login</Link></button>
+                    <button className='px-4 py-2 text-white uppercase bg-blue-400 border rounded-md hover:bg-transparent hover:text-blue-400'><Link to='/login'>Login</Link></button>
                     <button className='px-4 py-2 text-blue-400 uppercase border border-blue-400 rounded-md hover:bg-blue-400 hover:text-white'><Link to='/signup'>SignUp</Link></button>
                 </div>
             </div>
